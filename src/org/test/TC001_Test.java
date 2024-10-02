9ef57e2ee3829c12e98d8c3ae53b46bc6ea8c2a4
karthikkannan8098 <sathish48raju@gmail.com>
2024-10-02 10:29:49 +0530

    QA2

--- src/org/test/TC001_Test.java
+++ src/org/test/TC001_Test.java
@@ -3,7 +3,7 @@ package org.test;
 public class TC001_Test {
 	
 	public static void main(String[] args) {
-		System.out.println("Im from master branch");
+		System.out.println("Im from QA2 branch");
 	}
 	
 	
